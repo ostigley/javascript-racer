e6b4a512945ce2f4a7f901d2303ece7ba15a983c
Oliver Stigley <oliverstigley@gmail.com>
2016-03-21 16:22:27 +1300

    javascript cleaning and refactoring

--- myscript.js
+++ myscript.js
@@ -1,40 +1,34 @@
 
+
+//  Start function:   This function sets initial positions to the start point of the table.  And resets active positions to "". 
 var start = function() {
 	
-	//  clear active positions
+	//  clear active positions from the board. 
 	var positions = document.getElementsByClassName("active");
-	for (i=0; i<positions.length ; i++) {
+	for (i=positions.length-1; i>-1 ; i--) {
+	//iteration is reversed because the position array reduces in size by 1 each iteration. 
+		console.log(positions[i].classList);
 		positions[i].classList.remove("active");
 	}
 
-	var positions = document.getElementsByClassName("active");
-	for (i=0; i<positions.length ; i++) {
-		positions[i].classList.remove("active");
-	}
-
-	//  Put players at start line
-	// for (i=0; i<strips.length ; i++) {
-	// 	document.getElementById(strips[i]).children[0].classList.add("active"); 
-	// }
-
+	//  Put players back to start position. 
 	for(var key in players) {
 		document.getElementById(players[key].stripID).children[0].classList.add("active"); 
-
 	}
 
-
+	// call set=Pos to define position variables. 
 	setPos();
 	finisher = false;
 
+	//   initiate event listener for movements
+	document.addEventListener("keyup", keypress, false);
 }
 
 var setPos = function (replay) {
-	
+	// re-initialise position variables in pplayer object. 
 	players['Player 1'].position = document.getElementsByClassName('active')[0]; 
 	players['Player 2'].position = document.getElementsByClassName('active')[1];
-
 	// [0] index is returning the table entry that is currently active.  there sohuld only be two entries that are avtive on the whole page. 
-
 }
 
 var whichPlayer = function (player){
@@ -43,42 +37,35 @@ var whichPlayer = function (player){
 	} else {
 		return players.player2;
 	}
-
 }
 
-
 var finished = function(player) {
-	// var input;
-	// if (player === 'Player 1') {
-	// 	input = player1Pos;
-	// } else {
-	// 	input	= player2Pos;
-	// }
-
-	// input = whichPlayer(player)[0];
-
+//Check if player has crossed the finishe line.  Ask for new game.  Update score. 
 
 	if (players[player].position.classList[0] == "finish") {
+		finisher = true;
+		
 		// update score
 		players[player].score++;
 		console.log('Player 1 score: '+players['Player 1'].score +'\n'+ 'Player 2 score: '+players['Player 2'].score)
-		
-		// console.log(players[player].score;);
 
-		finisher = true;
+		//Ask to replay. 
 		var replay = confirm(player+ ' wins!!!\n \nDo you want a re-match?');
 		if (replay) {
 			start();
-
 		}
-
 	}
 };
 
 var updatePosition = function (player) {
+	// move player forward on the board
 	players[player].position.nextElementSibling.classList.add("active");
 	players[player].position.classList.remove("active");
+	
+	// re-initialise position variables in pplayer object. 
 	setPos();
+
+	//check if finished:
 	finished(player);
 }
 
@@ -97,6 +84,13 @@ var keypress = function (key) {
 }
 
 //   Initiate GAME PLAY!   
+/*
+
+1)____ Set up players & set finisher to false.
+
+2)____ Initiate start function
+
+*/
 
 var players = {
 	'Player 1': {
@@ -111,19 +105,9 @@ var players = {
 	}
 }
 
-
-// 
 var finisher = false;
-var strips = ['player1_strip', 'player2_strip']
-
-// var player1Score = 0;
-// var player2Score = 0;
-
 
 
 start();
-//setPos();
-document.addEventListener("keyup", keypress, false);
-
 
 
